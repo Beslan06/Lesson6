fd1f76a43511fc0228cc121ce3730ba46d0fb365
Beslan06 <bor.mm2011@yandex.ru>
2022-08-23 15:37:30 +0300

    задача 41

---
+++ Dz41/Program.cs
@@ -0,0 +1,7 @@
+﻿List numbers = new List();
+int m = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < m; i++) {
+numbers.Add(Convert.ToInt32(Console.ReadLine()));
+}
+Console.WriteLine(numbers.Where(n => n > 0).Count());
+Console.ReadKey();
